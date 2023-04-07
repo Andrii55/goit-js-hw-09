07ef1049495d9a29e884e108957339b3ead27475
Andrii55 <117086425+Andrii55@users.noreply.github.com>
2023-04-07 16:46:03 +0300

    addFirstTask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,30 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+const DELAY = 1000;
+
+const refs = {
+  btnStartStop: document.querySelectorAll('button[type="button"]'),
+};
+
+refs.btnStartStop.forEach(button => {
+  button.addEventListener('click', onHandleClick);
+});
+let intervalId;
+
+function onHandleClick(e) {
+  const btnStart = document.querySelector('[data-start]');
+  const btnStop = document.querySelector('[data-stop]');
+  if (btnStart === e.target && !intervalId) {
+    intervalId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      document.body.style.backgroundColor = randomColor;
+    }, DELAY);
+  } else if (btnStop === e.target && intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
   }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
